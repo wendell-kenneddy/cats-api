77ffcffe00b8bc3eeb5848790a837bdeda07ce0c
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-24 17:56:37 -0300

    feat(routes): add proper routing using controllers

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import { PersonsController } from "./controllers/PersonsController";
+import { SQLitePersonsRepository } from "./repositories/SQLitePersonRepository";
+import { dbPath } from "./db/dbPath";
+import { CatsController } from "./controllers/CatsController";
+import { SQLiteCatsRepository } from "./repositories/SQLiteCatsRepository";
 
 const router = Router();
+const personsController = new PersonsController(new SQLitePersonsRepository(dbPath));
+const catsController = new CatsController(new SQLiteCatsRepository(dbPath));
 
-router.get("/", (req, res) =>
-  res.status(200).json({
-    message: "Request received",
-  })
-);
+router.post("/persons", (req, res) => personsController.createOne(req, res));
+router.get("/persons/:id", (req, res) => personsController.getOne(req, res));
+router.get("/persons", (req, res) => personsController.getAll(req, res));
+router.delete("/persons/:id", (req, res) => personsController.deleteOne(req, res));
+router.put("/persons/:id", (req, res) => personsController.updateOne(req, res));
+
+router.post("/cats", (req, res) => catsController.createOne(req, res));
+router.get("/cats/:id", (req, res) => catsController.getOne(req, res));
+router.get("/cats", (req, res) => catsController.getAll(req, res));
+router.delete("/cats/:id", (req, res) => catsController.deleteOne(req, res));
+router.put("/cats/:id", (req, res) => catsController.updateOne(req, res));
 
 export { router };
