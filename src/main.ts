6d496b0993ab6383037e9746ed9056ed42e45f7f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-25 07:53:08 -0300

    feat(main.ts): use helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import dotenv from "dotenv";
 import { router } from "./router";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -7,6 +8,7 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet);
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorHandler);
